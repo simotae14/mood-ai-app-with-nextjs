79f1115a4b5749e55eb3311493bc27faea54cb2c
Simone Taeggi <startae14@gmail.com>
2024-06-22 17:42:46 +0200

    Homepage with Tailwind CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>home</div>
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">The best Journal app, period.</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          This is the best app for tracking your mood through out your life. All you have to do is be honest.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">get started</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
